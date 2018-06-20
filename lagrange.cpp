38a3c948762880b43d8a3864a7f9f67dfec13ae3
Skdi <jddhghf@gmail.com>
2018-06-20 10:14:02 -0500

    Update lagrange.cpp

--- lagrange.cpp
+++ lagrange.cpp
@@ -1,67 +1,50 @@
+#include <stdio.h>
 #include <iostream>
-#include <fstream>
 #include <cmath>
+#include <fstream>
+#include "math.h"
+#include <stdlib.h>
 using namespace std;
-//funcion para el calculo de las constantes a0
-float Aitmen(int i0, int i1, float* xAt, float* yAt) {
-    if(i1-i0 == 0) {//si el punto a comparar es el mismo con el ya obtenido 
-        return yAt[i0];//retornamos el punto
-    }
-    else {//sino
-        return (Aitmen(i0+1, i1, xAt, yAt) - Aitmen(i0, i1-1, xAt, yAt))/(xAt[i1]-xAt[i0]);//llamamos recursivamente a sus predecesores
-    }
-}
-//funcion para interpolar los puntos x,y
-float inter_new(float xAprox, int n, float* x, float* y) {
-    float yAprox;
-    float aitmenn[n];
-    //calculamos las constantes a0...an
-    for(int i = 0; i < n; i++) {
-        aitmenn[i] = Aitmen(0, i, x, y);
-    }
-    yAprox = 0;    
-    for(int i = 0; i < n; i++) {
-        float aprox = aitmenn[i];//trabajamos la constante ai
-        for(int j = 0; j < i; j++) {//aproximacion
-            aprox *= (xAprox - x[j]);
-        }
-        
-        yAprox += aprox;
-    }
-    
-    return yAprox;
-}
+ofstream archivo;
+#define pi 3.141592654
+#define ep 2.718281828
+float x[50],y[50],z,l, valor=0;
+
 void llenar_poli(float x[],float y[],int n){
-    cout<<"ingrese x: "<<endl;
-    for(int i=0; i<n; i++){
-        cin>>x[i];
+	cout<<"ingrese x: "<<endl;
+	for(int i=0; i<n; i++){
+ 		cin>>x[i];
     }
  
       cout<<endl;
       cout<<"ingrese y: "<<endl;
-    for(int i=0; i<n; i++){
+	for(int i=0; i<n; i++){
         cin>>y[i]; 
-    }
+	}
 }
-void main_newton(){
-    float xAprox, yAprox;
-    int n,m;
-    cout << "ingrese el numero de elementos: ";
-    cin >> n;
-    float x[n];
-    float y[n];
-    llenar_poli(x,y,n);
-    cout << "ingrese el numero de puntos a evaluar ";
-    cin>>m;
-    for(int i=0;i<m;i++){
-        cin >> xAprox;
-        yAprox = inter_new(xAprox, n, x, y);
-        cout << "f(x) = " << yAprox<<endl;
-    }
+void inter_lagrange(){
+	int n,m;
+	cout<<"ingrese el numero de elementos: "; cin>>n;
+	llenar_poli(x,y,n);
+	cout<<endl;
+	cout<<"ingrese el numero de puntos a evaluar";
+	cin>>m;cout<<endl;
+	for(int i=0;i<m;i++){
+		cin>>z;
+		for(int i=0; i<n ;i++){
+		    l=y[i;]
+		    for(int j=0; j<n; j++){
+		        if(i!=j){
+		            l=(l*(z-x[j]))/(x[i]-x[j]);
+		        }
+		    }
+		    valor=valor+l;
+		}
+		cout<<endl<<"Z= "<<z <<" es : "<<valor<<endl;
+	}
+}
 
-}
-int main() {
-    main_newton();
-
-    
-}
+int main(){
+	inter_lagrange();
+	return 0;
+ }
